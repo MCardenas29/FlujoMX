a75876cf643f313649844f3a71d847d01485f74e
Mauricio <madjesc@suavemente.madjesc.me>
2024-10-29 10:12:23 -0600

    feat(mqtt): Added mqtt

--- lib/services/flow.dart
+++ lib/services/flow.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
 import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+final client = MqttServerClient.withPort("10.0.2.2", "", 1883);
 
 Future<void> initServices() async {
   final service = FlutterBackgroundService();
-  await service.configure(
+  var configured = await service.configure(
     androidConfiguration: AndroidConfiguration(
         autoStart: true,
         isForegroundMode: false,
@@ -10,12 +15,22 @@ Future<void> initServices() async {
         onStart: startService),
     iosConfiguration: IosConfiguration(),
   );
+  print(configured ? "Configurado" : "No configurado");
   return;
 }
 
 @pragma("vm:entry-point")
-void startService(ServiceInstance service) async {
-  print("OnStart");
+Future<void> startService(ServiceInstance service) async {
+  print("Pepe");
+  print("Pepo");
+  var status = await client.connect();
+  print("Pepa");
+
+  client.subscribe("flow/add", MqttQos.atMostOnce);
+  client.published!.listen((MqttPublishMessage message) {
+    print(message.payload.message.toString());
+  });
   service.on("stop").listen((ev) => print("on stop"));
   service.on("start").listen((ev) => print("on start"));
+  return;
 }
